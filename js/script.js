65c421bd2103c7090d4d99ac22ce81f486fddbfe
xgoldzin <gustafera34@gmail.com>
2024-10-02 16:30:10 -0300

    last commit

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const peopleInput = document.getElementById('peopleInput');
 const addButton = document.getElementById('addButton');
 
 addButton.addEventListener('click', () => {
-    let peopleToAdd = parseInt(peopleInput.value);
+    let peopleToAdd = Number(peopleInput.value);
     let peopleAdded = 0; // Contador de pessoas que foram realmente adicionadas
 
     while (peopleToAdd > 0 && currentCount < maxCapacity) {
@@ -35,7 +35,7 @@ const completeButton = document.getElementById('completeButton');
 const totalAmountElement = document.getElementById('totalAmount');
 
 insertButton.addEventListener('click', () => {
-    let productValue = parseFloat(productInput.value);
+    let productValue =Number(productInput.value);
     
     if (!isNaN(productValue) && productValue > 0) {
         totalAmount += productValue;
@@ -49,3 +49,40 @@ completeButton.addEventListener('click', () => {
     insertButton.disabled = true;
     productInput.disabled = true; // Opcional: desabilita o input também
 });
+
+function calcProgGeom() {
+    let inicio = Number(document.getElementById("inicio").value);
+    let passo = Number(document.getElementById("passo").value);
+    let repeticoes = Number(document.getElementById("repeticoes").value);
+
+    let result = inicio; // O primeiro termo é o valor inicial
+    document.getElementById("progGeom").innerHTML = ""; // Limpa resultados anteriores
+
+   let i = 0; // Inicializa o contador
+    while (i < repeticoes) {
+        document.getElementById("progGeom").innerHTML += `<p>${result}</p>`;
+        result *= passo; // Calcula o próximo termo da PG
+        i++; // Incrementa o contador
+    }
+}
+
+function calcularPA() {
+    let primeiroTermo = Number(document.getElementById("inputPrimeiroTermo").value);
+    let razao = Number(document.getElementById("inputRazao").value);
+    let quantidadeTermos = Number(document.getElementById("inputTermos").value);
+
+    // Verifica se os valores de entrada são válidos
+    if (isNaN(primeiroTermo) || isNaN(razao) || isNaN(quantidadeTermos) || quantidadeTermos < 1) {
+        document.getElementById("divResultadoPA").innerHTML = "<p>Por favor, insira valores válidos.</p>";
+        return;
+    }
+
+    let resultadoPA = ""; // Inicializa a string para os termos da PA
+
+    for (let i = 0; i < quantidadeTermos; i++) {
+        let termoAtual = primeiroTermo + i * razao; // Calcula o termo atual
+        resultadoPA += `<p>${termoAtual}</p>`; // Adiciona o termo à string
+    }
+
+    document.getElementById("divResultadoPA").innerHTML = resultadoPA; // Exibe os termos da PA
+}
